7c2712a177c4f8a0754a940a7bfd198d976e9f9d
Victor Antonio Martinez Solis <victormartinezsolis20@gmail.com>
2023-10-28 20:14:41 -0600

    🛡️ Protección de componentes con guards, panel administrador

--- admin/src/app/services/admin.service.ts
+++ admin/src/app/services/admin.service.ts
@@ -2,27 +2,51 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GLOBAL } from './GLOBAL';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminService {
-
   public url;
 
-  constructor(
-    private _http: HttpClient,
-  ) {
+  constructor(private _http: HttpClient) {
     this.url = GLOBAL.url;
   }
 
-  login_admin(data: { email: any; password: any; }):Observable<any>{
-    let headers = new HttpHeaders().set('Content-Type','application/json');
-    return this._http.post(this.url+'login_admin',data,{headers:headers});
-
+  login_admin(data: { email: any; password: any }): Observable<any> {
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this._http.post(this.url + 'login_admin', data, {
+      headers: headers,
+    });
   }
 
-  getToken(){
+  getToken() {
     return localStorage.getItem('token');
+  }
+
+  public isAuthenticated(allowRoles: string[]): boolean {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const helper = new JwtHelperService();
+      var decodedToken = helper.decodeToken(token);
+
+      console.log(decodedToken);
+
+      if (!decodedToken) {
+        console.log('NO ES VALIDO');
+        localStorage.removeItem('token');
+        return false;
+      }
+    } catch (error) {
+      localStorage.removeItem('token');
+      return false;
+    }
+
+    return allowRoles.includes(decodedToken['role']);
   }
 }
