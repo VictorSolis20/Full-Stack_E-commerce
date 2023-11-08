91298b454f29d67e681b45ef0b0eab03183b3f69
Victor Antonio Martinez Solis <victormartinezsolis20@gmail.com>
2023-11-07 21:05:01 -0600

    📝 Registro de cupón

---
+++ admin/src/app/services/cupon.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GLOBAL } from './GLOBAL';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CuponService {
+
+  public url;
+
+  constructor(private _http: HttpClient) {
+    this.url = GLOBAL.url;
+  }
+
+  registro_cupon_admin(
+    data: any,
+    token: string | number | null
+  ): Observable<any> {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: token !== null ? token.toString() : '',
+    });
+    return this._http.post(
+      this.url + 'registro_cupon_admin',data,
+      {
+        headers: headers,
+      }
+    );
+  }
+}
