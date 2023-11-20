79b4f9fd62ed343cc6f582b785c876e87d12c46b
Victor Antonio Martinez Solis <victormartinezsolis20@gmail.com>
2023-11-19 18:10:37 -0600

    🎚️ Filtrar productos entre un rango de precios

--- tienda/src/app/components/productos/index-producto/index-producto.component.ts
+++ tienda/src/app/components/productos/index-producto/index-producto.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GLOBAL } from 'src/app/services/GLOBAL';
 import { ClienteService } from 'src/app/services/cliente.service';
-declare var noUiSlider : any;
+declare var noUiSlider: any;
 declare var $: any;
 declare var jQuery: any;
 // import noUiSlider from 'nouislider';
@@ -13,85 +13,108 @@ declare var jQuery: any;
 })
 export class IndexProductoComponent implements OnInit {
 
-  public config_global : any = {};
+  public config_global: any = {};
   public filter_categoria = '';
-  public productos : Array<any> = [];
+  public productos: Array<any> = [];
   public filter_producto = '';
   public url;
   public load_data = true;
-  
+
   constructor(
-    private _clienteService : ClienteService
-  ){
+    private _clienteService: ClienteService
+  ) {
     this.url = GLOBAL.url;
     this._clienteService.obtener_config_publico().subscribe(
-      response=>{
+      response => {
         this.config_global = response.data;
-        
+
       }
     )
 
     this._clienteService.listar_productos_publico(this.filter_producto).subscribe(
-      response=>{
+      response => {
         this.productos = response.data;
         this.load_data = false;
-        
+
       }
-    )
+    );
   }
 
   ngOnInit(): void {
 
-    var slider : any = document.getElementById('slider');
+    var slider: any = document.getElementById('slider');
 
     noUiSlider.create(slider, {
-        start: [0, 1000],
-        connect: true,
-        decimals: false,
-        range: {
-            'min': 0,
-            'max': 1000
-        },
-        tooltips: [true,true],
-        pips: {
-          mode: 'count', 
-          values: 5,
-          
-        }
+      start: [0, 50000],
+      connect: true,
+      decimals: false,
+      range: {
+        'min': 0,
+        'max': 50000
+      },
+      tooltips: [true, true],
+      pips: {
+        mode: 'count',
+        values: 5,
+
+      }
     })
 
     slider.noUiSlider.on('update', function (values: any) {
-      
-        $('.cs-range-slider-value-min').val(values[0]);
-        $('.cs-range-slider-value-max').val(values[1]);
+
+      $('.cs-range-slider-value-min').val(values[0]);
+      $('.cs-range-slider-value-max').val(values[1]);
     });
-    $('.noUi-tooltip').css('font-size','11px');
+    $('.noUi-tooltip').css('font-size', '11px');
   }
 
-  buscar_categorias(){
-    if(this.filter_categoria){
+  buscar_categorias() {
+    if (this.filter_categoria) {
       var search = new RegExp(this.filter_categoria, 'i');
       this.config_global.categorias = this.config_global.categorias.filter(
-        (item: { titulo: string; })=>search.test(item.titulo)
+        (item: { titulo: string; }) => search.test(item.titulo)
       );
-    }else{
+    } else {
       this._clienteService.obtener_config_publico().subscribe(
-        response=>{
+        response => {
           this.config_global = response.data;
-          
+
         }
       )
     }
   }
 
-  buscar_producto(){
+  buscar_producto() {
     this._clienteService.listar_productos_publico(this.filter_producto).subscribe(
-      response=>{
+      response => {
         this.productos = response.data;
         this.load_data = false;
-        
+
       }
     )
+  }
+
+  buscar_precios() {
+
+    this._clienteService.listar_productos_publico(this.filter_producto).subscribe(
+      response => {
+        this.productos = response.data;
+        let min = parseInt($('.cs-range-slider-value-min').val());
+        let max = parseInt($('.cs-range-slider-value-max').val());
+
+        console.log(min);
+        console.log(max);
+
+        this.productos = this.productos.filter((item) => {
+          return item.precio >= min &&
+            item.precio <= max
+        });
+
+      }
+    );
+
+
+
   }
 
 }
