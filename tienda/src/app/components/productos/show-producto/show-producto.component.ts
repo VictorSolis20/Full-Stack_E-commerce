5ecbb92dea22621c3d363a481563f3ef0645c71a
Victor Antonio Martinez Solis <victormartinezsolis20@gmail.com>
2023-11-21 14:55:05 -0600

    🖼️ Galería de producto dinámico

--- tienda/src/app/components/productos/show-producto/show-producto.component.ts
+++ tienda/src/app/components/productos/show-producto/show-producto.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GLOBAL } from 'src/app/services/GLOBAL';
 import { GuestService } from 'src/app/services/guest.service';
-declare var tns:any;
-declare var lightGallery:any;
+declare var tns: any;
+declare var lightGallery: any;
 
 @Component({
   selector: 'app-show-producto',
@@ -13,51 +13,55 @@ declare var lightGallery:any;
 export class ShowProductoComponent implements OnInit {
 
   public slug: any;
-  public producto : any = {};
+  public producto: any = {};
   public url;
 
   constructor(
-    private _route : ActivatedRoute,
-    private _guestService : GuestService
-  ){
+    private _route: ActivatedRoute,
+    private _guestService: GuestService
+  ) {
     this.url = GLOBAL.url;
     this._route.params.subscribe(
-      params=>{
+      params => {
         this.slug = params['slug'];
-        
+
         this._guestService.obtener_productos_slug_publico(this.slug).subscribe(
-          response=>{
+          response => {
             this.producto = response.data;
-            
+
           }
         )
-        
+
       }
     );
   }
 
   ngOnInit(): void {
-    tns({
-      container: '.cs-carousel-inner',
-      controlsText: ['<i class="cxi-arrow-left"></i>', '<i class="cxi-arrow-right"></i>'],
-      navPosition: "top",
-      controlsPosition: "top",
-      mouseDrag: !0,
-      speed: 600,
-      autoplayHoverPause: !0,
-      autoplayButtonOutput: !1,
-      navContainer: "#cs-thumbnails",
-      navAsThumbnails: true,
-      gutter: 15,
-    });
 
-    var e = document.querySelectorAll(".cs-gallery");
-    if (e.length){
-      for (var t = 0; t < e.length; t++){
-        lightGallery(e[t], { selector: ".cs-gallery-item", download: !1, videojs: !0, youtubePlayerParams: { modestbranding: 1, showinfo: 0, rel: 0 }, vimeoPlayerParams: { byline: 0, portrait: 0 } });
+    setTimeout(() => {
+      tns({
+        container: '.cs-carousel-inner',
+        controlsText: ['<i class="cxi-arrow-left"></i>', '<i class="cxi-arrow-right"></i>'],
+        navPosition: "top",
+        controlsPosition: "top",
+        mouseDrag: !0,
+        speed: 600,
+        autoplayHoverPause: !0,
+        autoplayButtonOutput: !1,
+        navContainer: "#cs-thumbnails",
+        navAsThumbnails: true,
+        gutter: 15,
+      });
+
+      var e = document.querySelectorAll(".cs-gallery");
+      if (e.length) {
+        for (var t = 0; t < e.length; t++) {
+          lightGallery(e[t], { selector: ".cs-gallery-item", download: !1, videojs: !0, youtubePlayerParams: { modestbranding: 1, showinfo: 0, rel: 0 }, vimeoPlayerParams: { byline: 0, portrait: 0 } });
+        }
       }
-    }
+    }, 500)
 
+    
     tns({
       container: '.cs-carousel-inner-two',
       controlsText: ['<i class="cxi-arrow-left"></i>', '<i class="cxi-arrow-right"></i>'],
