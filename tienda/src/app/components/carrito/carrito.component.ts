733f045fa347a4b38055c33dcccc5bd388f0a4ea
Victor Antonio Martinez Solis <victormartinezsolis20@gmail.com>
2023-12-03 19:04:49 -0600

    💳 Validar tarjetas de crédito, librerias externas

--- tienda/src/app/components/carrito/carrito.component.ts
+++ tienda/src/app/components/carrito/carrito.component.ts
@@ -4,6 +4,8 @@ import { ClienteService } from 'src/app/services/cliente.service';
 import { io } from "socket.io-client";
 
 declare var iziToast: any;
+declare var Cleave: any;
+declare var StickySidebar: any;
 
 @Component({
   selector: 'app-carrito',
@@ -15,7 +17,7 @@ export class CarritoComponent implements OnInit {
   public idcliente;
   public token;
 
-  public carrito_arr : Array<any> = [];
+  public carrito_arr: Array<any> = [];
   public url;
   public subtotal = 0;
   public total_pagar = 0;
@@ -23,14 +25,14 @@ export class CarritoComponent implements OnInit {
 
   constructor(
     private _clienteService: ClienteService
-  ){
+  ) {
 
     this.idcliente = localStorage.getItem('_id');
     this.token = localStorage.getItem('token');
     this.url = GLOBAL.url;
 
-    this._clienteService.obtener_carrito_cliente(this.idcliente,this.token).subscribe(
-      response=>{
+    this._clienteService.obtener_carrito_cliente(this.idcliente, this.token).subscribe(
+      response => {
         this.carrito_arr = response.data;
         this.calcular_carrito();
       }
@@ -38,19 +40,33 @@ export class CarritoComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    
+    setTimeout(() => {
+      new Cleave('#cc-number', {
+        creditCard: true,
+        onCreditCardTypeChanged: function (type: any) {
+          // update UI ...
+        }
+      });
+
+      new Cleave('#cc-exp-date', {
+        date: true,
+        datePattern: ['m', 'y']
+      });
+
+      var sidebar = new StickySidebar('.sidebar-sticky', {topSpacing: 20});
+    })
   }
 
-  calcular_carrito(){
+  calcular_carrito() {
     this.carrito_arr.forEach(element => {
       this.subtotal = this.subtotal + parseInt(element.producto.precio);
     });
     this.total_pagar = this.subtotal;
   }
 
-  eliminar_item(id: any){
-    this._clienteService.eliminar_carrito_cliente(id,this.token).subscribe(
-      response=>{
+  eliminar_item(id: any) {
+    this._clienteService.eliminar_carrito_cliente(id, this.token).subscribe(
+      response => {
         iziToast.show({
           title: 'SUCCESS',
           titleColor: '#1DC74C',
@@ -59,14 +75,14 @@ export class CarritoComponent implements OnInit {
           position: 'topRight',
           message: 'Se eliminó el producto correctamente.'
         });
-        this.socket.emit('delete-carrito',{data:response.data});
-        this._clienteService.obtener_carrito_cliente(this.idcliente,this.token).subscribe(
-          response=>{
+        this.socket.emit('delete-carrito', { data: response.data });
+        this._clienteService.obtener_carrito_cliente(this.idcliente, this.token).subscribe(
+          response => {
             this.carrito_arr = response.data;
             this.calcular_carrito();
           }
         );
-        
+
       }
     )
   }
