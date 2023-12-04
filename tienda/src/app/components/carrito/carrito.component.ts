aeacb9df3aef6273240c2877b982ba009e8722b2
Victor Antonio Martinez Solis <victormartinezsolis20@gmail.com>
2023-12-03 22:35:34 -0600

    💳 Instalación de Paypal Checkout

--- tienda/src/app/components/carrito/carrito.component.ts
+++ tienda/src/app/components/carrito/carrito.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { GLOBAL } from 'src/app/services/GLOBAL';
 import { ClienteService } from 'src/app/services/cliente.service';
 import { io } from "socket.io-client";
@@ -7,6 +7,11 @@ import { GuestService } from 'src/app/services/guest.service';
 declare var iziToast: any;
 declare var Cleave: any;
 declare var StickySidebar: any;
+declare var paypal: any;
+
+interface HtmlInputEvent extends Event{
+  target : HTMLInputElement & EventTarget;
+}
 
 @Component({
   selector: 'app-carrito',
@@ -14,7 +19,7 @@ declare var StickySidebar: any;
   styleUrls: ['./carrito.component.css']
 })
 export class CarritoComponent implements OnInit {
-
+  @ViewChild('paypalButton',{static:true}) paypalElement : ElementRef | any;
   public idcliente;
   public token;
 
@@ -70,6 +75,36 @@ export class CarritoComponent implements OnInit {
     });
 
     this.get_direccion_principal();
+
+    paypal.Buttons({
+      style: {
+          layout: 'horizontal'
+      },
+      createOrder: (data:any,actions:any)=>{
+  
+          return actions.order.create({
+            purchase_units : [{
+              description : 'Nombre del pago',
+              amount : {
+                currency_code : 'USD',
+                value: 999
+              },
+            }]
+          });
+        
+      },
+      onApprove : async (data:any,actions:any)=>{
+        const order = await actions.order.capture();
+  
+        
+      },
+      onError : (err: any) =>{
+       
+      },
+      onCancel: function (data:any, actions:any) {
+        
+      }
+    }).render(this.paypalElement.nativeElement);
   }
 
   get_direccion_principal(){
