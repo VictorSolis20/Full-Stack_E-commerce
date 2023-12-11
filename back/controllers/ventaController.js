c936c99706f50f86e9279bad16d678235114f33b
Victor Antonio Martinez Solis <victormartinezsolis20@gmail.com>
2023-12-10 19:59:03 -0600

    🛍️ Disminución de stock en la compra

--- back/controllers/ventaController.js
+++ back/controllers/ventaController.js
@@ -1,5 +1,6 @@
 var Venta = require('../models/venta');
 var Dventa = require('../models/dventa');
+var Producto = require('../models/producto');
 
 const registro_compra_cliente = async function(req,res){
     if(req.user){
@@ -38,14 +39,21 @@ const registro_compra_cliente = async function(req,res){
 
         console.log(data);
         
-        // let venta = await Venta.create(data);
+        let venta = await Venta.create(data);
 
-        // detalles.forEach(async element => {
-        //     element.venta = venta._id;
-        //     await Dventa.create(element);
-        // });
+        detalles.forEach(async element => {
+            element.venta = venta._id;
+            await Dventa.create(element);
 
-        // res.status(200).send({venta:venta});
+            let element_producto = await Producto.findById({_id:element.producto});
+            let new_stock = element_producto.stock - element.cantidad;
+
+            await Producto.findByIdAndUpdate({_id: element.producto},{
+                stock: new_stock
+            });
+        });
+
+        res.status(200).send({venta:venta});
     }else{
         res.status(500).send({ message: 'NoAccess' });
     }
